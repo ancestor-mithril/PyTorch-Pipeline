0342e0d049c61555160c5da8c1323f71efc22046
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2024-04-28 21:52:42 +0300

    Fixed BS scheduler parameters

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 git clone https://github.com/ancestor-mithril/PyTorch-Pipeline.git
 cd PyTorch-Pipeline
 PYTHONOPTIMIZE=2 python .\main.py -device cuda:0 -lr 0.001 -bs 10 -epochs 200 -dataset cifar10 -data_path ../data -scheduler ReduceLROnPlateau -scheduler_params "{'mode':'min', 'factor':0.5}" -model preresnet18_c10 -fill 0.5 --cutout --autoaug --tta --half
-PYTHONOPTIMIZE=2 python .\main.py -device cuda:0 -lr 0.001 -bs 10 -epochs 200 -dataset cifar10 -data_path ../data -scheduler IncreaseBSOnPlateau -scheduler_params "{'mode':'min', 'factor':2}" -model preresnet18_c10 -fill 0.5 --cutout --autoaug --tta --half
+PYTHONOPTIMIZE=2 python .\main.py -device cuda:0 -lr 0.001 -bs 10 -epochs 200 -dataset cifar10 -data_path ../data -scheduler IncreaseBSOnPlateau -scheduler_params "{'mode':'min', 'factor':2.0, 'max_batch_size': 1000}" -model preresnet18_c10 -fill 0.5 --cutout --autoaug --tta
 ```
--- main.py
+++ main.py
@@ -35,5 +35,6 @@ if __name__ == '__main__':
     print(args)
     Trainer(args).run()
 
+# TODO: Factor could be int
 # PYTHONOPTIMIZE=2 python .\main.py -device cuda:0 -lr 0.001 -bs 10 -epochs 200 -dataset cifar10 -data_path ../data -scheduler ReduceLROnPlateau -scheduler_params "{'mode':'min', 'factor':0.5}" -model preresnet18_c10 -fill 0.5 --cutout --autoaug --tta --half # noqa: E501
-# PYTHONOPTIMIZE=2 python .\main.py -device cuda:0 -lr 0.001 -bs 10 -epochs 200 -dataset cifar10 -data_path ../data -scheduler IncreaseBSOnPlateau -scheduler_params "{'mode':'min', 'factor':2}" -model preresnet18_c10 -fill 0.5 --cutout --autoaug --tta --half # noqa: E501
+# PYTHONOPTIMIZE=2 python .\main.py -device cuda:0 -lr 0.001 -bs 10 -epochs 200 -dataset cifar10 -data_path ../data -scheduler IncreaseBSOnPlateau -scheduler_params "{'mode':'min', 'factor':2.0, 'max_batch_size': 1000}" -model preresnet18_c10 -fill 0.5 --cutout --autoaug --tta --half # noqa: E501
