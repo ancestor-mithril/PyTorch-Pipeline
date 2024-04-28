518b7ebfa4b4a35cec38f141d5b13252ec213b7b
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2024-04-28 21:31:20 +0300

    Added bs_scheduler

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -1,8 +1,14 @@
+# pip install git+https://github.com/ancestor-mithril/bs_scheduler.git@master
+from bs_scheduler import IncreaseBSOnPlateau
+from torch.optim import Optimizer
 from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.utils.data import DataLoader
 
 
-def init_scheduler(args, optimizer, train_loader):
-    if args.scheduler == 'ReduceLROnPlateau':
+def init_scheduler(args, optimizer: Optimizer, train_loader: DataLoader):
+    if args.scheduler == 'IncreaseBSOnPlateau':
+        scheduler = IncreaseBSOnPlateau(train_loader, **args.scheduler_params)
+    elif args.scheduler == 'ReduceLROnPlateau':
         scheduler = ReduceLROnPlateau(optimizer, **args.scheduler_params)
     else:
         raise NotImplementedError(f'Scheduler {args.scheduler} not implemented')
