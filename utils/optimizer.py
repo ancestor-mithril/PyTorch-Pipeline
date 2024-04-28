d5dfe66998fe57dd9fa4276519ca532ce4586777
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2024-04-28 21:43:29 +0300

    Added todo

--- utils/optimizer.py
+++ utils/optimizer.py
@@ -3,6 +3,7 @@ from torch.optim import Optimizer
 
 
 def init_optimizer(args, parameters) -> Optimizer:
+    # TODO: Add fused
     momentum = 0.9 if not hasattr(args, 'momentum') else args.momentum
     weight_decay = 5e-4 if not hasattr(args, 'weight_decay') else args.weight_decay
 
