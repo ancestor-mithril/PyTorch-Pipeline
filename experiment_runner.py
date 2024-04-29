fdc57a1641a2e4366f67864c1eb6a25fbfd3c034
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2024-04-29 16:54:56 +0300

    Fixed experiment runner

--- experiment_runner.py
+++ experiment_runner.py
@@ -65,7 +65,7 @@ def create_run(dataset, model, optimizer, seed, epochs, es_patience, batch_size,
         f" --cutout"
         f" --autoaug"
         f" --tta"
-    ) + " --half" if torch.cuda.is_available() else ""
+    ) + (" --half" if torch.cuda.is_available() else "")
 
 
 def generate_runs():
@@ -82,7 +82,7 @@ def generate_runs():
         2525
     ]
     epochss = [
-        10
+        200
     ]
     es_patiences = [
         20
