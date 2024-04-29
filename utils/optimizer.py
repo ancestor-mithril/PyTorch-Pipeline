64fe6f1cc286e74240cc4365e1a5812ff5956ce4
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2024-04-29 14:30:11 +0300

    Using fused step for optimizers

--- utils/optimizer.py
+++ utils/optimizer.py
@@ -1,3 +1,4 @@
+import torch
 from torch import optim
 from torch.optim import Optimizer
 
@@ -7,4 +8,15 @@ def init_optimizer(args, parameters) -> Optimizer:
     momentum = 0.9 if not hasattr(args, 'momentum') else args.momentum
     weight_decay = 5e-4 if not hasattr(args, 'weight_decay') else args.weight_decay
 
-    return optim.SGD(parameters, lr=args.lr, momentum=momentum, weight_decay=weight_decay)
+    kwargs = {
+        'lr': args.lr,
+        'momentum': momentum,
+        'weight_decay': weight_decay,
+    }
+    if torch.torch_version.TorchVersion(torch.__version__) >= '2.3.0':
+        kwargs['fused'] = True
+    else:
+        import warnings
+        warnings.warn("Upgrade torch to support fused optimizers")
+
+    return optim.SGD(parameters, **kwargs)
