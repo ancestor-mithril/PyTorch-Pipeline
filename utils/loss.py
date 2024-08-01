5cf250402056808834ae367bda6c5568217481d1
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2024-08-01 17:21:47 +0300

    Updated criterion to prepare for no loss aggregation

--- utils/loss.py
+++ utils/loss.py
@@ -2,7 +2,10 @@ from torch import nn
 
 
 def init_criterion(args):
-    if not hasattr(args, 'criterion'):
-        return nn.CrossEntropyLoss()
+    criterion = args.criterion if hasattr(args, 'criterion') else 'CrossEntropyLoss'
+    reduction = args.reduction if hasattr(args, 'reduction') else 'mean'
+
+    if criterion == 'crossentropy':
+        return nn.CrossEntropyLoss(reduction=reduction)
 
     raise NotImplementedError(f'Criterion {args.criterion} not implemented')
